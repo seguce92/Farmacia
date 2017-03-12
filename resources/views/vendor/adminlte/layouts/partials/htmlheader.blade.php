5b64e7028c4dd10579d877f84381257561db2472
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-12 13:39:53 -0600

    Cambio orden mas abajo librería bootstrap

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -8,17 +8,15 @@
 {{--    <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />--}}
 
 
-    <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="{{asset("/css/bootstrap.min.css")}}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset("/css/font-awesome.css")}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">--}}
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset("/css/AdminLTE.min.css")}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
+    folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{asset("/css/skins/_all-skins.min.css")}}">
     <!-- iCheck -->
     <link rel="stylesheet" href="{{asset("plugins/iCheck/square/blue.css")}}">
@@ -26,6 +24,8 @@
     <link rel="stylesheet" href="{{asset("plugins/morris/morris.css")}}">
     <!-- jvectormap -->
     <link rel="stylesheet" href="{{asset("plugins/jvectormap/jquery-jvectormap-1.2.2.css")}}">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="{{asset("/css/bootstrap.min.css")}}">
     <!-- Date Picker -->
     <link rel="stylesheet" href="{{asset("plugins/datepicker/datepicker3.css")}}">
     <!-- Daterange picker -->
