65b61ca7f4085c382e290c91812dc703044418f5
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-13 12:05:48 -0600

    Descargada librería ioniconos y comentadas algunas en plantilla master que no son nescesarias para la funcionalidad principal

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -5,35 +5,37 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-{{--    <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />--}}
+    {{--<link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />--}}
 
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset("/css/font-awesome.css")}}">
-{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">--}}
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset("/css/AdminLTE.min.css")}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
     folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{asset("/css/skins/_all-skins.min.css")}}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{asset("plugins/iCheck/square/blue.css")}}">
-    <!-- Morris chart -->
-    <link rel="stylesheet" href="{{asset("plugins/morris/morris.css")}}">
-    <!-- jvectormap -->
-    <link rel="stylesheet" href="{{asset("plugins/jvectormap/jquery-jvectormap-1.2.2.css")}}">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset("/css/font-awesome.css")}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset("css/ionicons.min.css")}}">
+    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">--}}
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="{{asset("/css/bootstrap.min.css")}}">
-    <!-- Date Picker -->
-    <link rel="stylesheet" href="{{asset("plugins/datepicker/datepicker3.css")}}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{asset("plugins/daterangepicker/daterangepicker.css")}}">
-    <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="{{asset("plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css")}}">
-
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{asset("plugins/iCheck/square/blue.css")}}">
+    <!--Data tables-->
     <link rel="stylesheet" href="{{asset('plugins/DataTables/datatables.min.css')}}">
+
+    <!-- Morris chart -->
+    {{--<link rel="stylesheet" href="{{asset("plugins/morris/morris.css")}}">--}}
+    <!-- jvectormap -->
+    {{--<link rel="stylesheet" href="{{asset("plugins/jvectormap/jquery-jvectormap-1.2.2.css")}}">--}}
+    <!-- Date Picker -->
+    {{--<link rel="stylesheet" href="{{asset("plugins/datepicker/datepicker3.css")}}">--}}
+    <!-- Daterange picker -->
+    {{--<link rel="stylesheet" href="{{asset("plugins/daterangepicker/daterangepicker.css")}}">--}}
+    <!-- bootstrap wysihtml5 - text editor -->
+    {{--<link rel="stylesheet" href="{{asset("plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css")}}">--}}
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
