74ae33599928b8fedd199ae4d82b9de682e3566d
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-17 11:56:27 -0600

    Creado seeder para roles y asignados al usuario admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(OptionTableSeeder::class);
+        $this->call(RolsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,8 +18,14 @@ class UsersTableSeeder extends Seeder
             "password" => bcrypt("admin")
         ]);
 
+        //asigna todas las opciones al usuario 1
         User::findOrFail(1)->opciones()->sync([
             1,2,3,4,5,6
+        ]);
+
+        //asigna todos los roles al usuario 1
+        User::findOrFail(1)->rols()->sync([
+            1,2,3
         ]);
     }
 }
