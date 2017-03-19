e4b3f6bfe6441f5999be32b8f2e4638b7fdfcf36
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-18 22:53:43 -0600

    Seeder para mas opciones

--- database/seeds/OptionTableSeeder.php
+++ database/seeds/OptionTableSeeder.php
@@ -12,6 +12,7 @@ class OptionTableSeeder extends Seeder
      */
     public function run()
     {
+        //1
         Option::create([
             "nombre"=>"Admin",
             "descripcion"=>"Opciones de administración",
@@ -19,6 +20,7 @@ class OptionTableSeeder extends Seeder
             "orden"=>"1",
         ]);
 
+        //2
         Option::create([
             "padre"=>"1",
             "nombre"=>"Usuarios",
@@ -28,6 +30,7 @@ class OptionTableSeeder extends Seeder
             "icono_r"=>""
         ]);
 
+        //3
         Option::create([
             "padre"=>"1",
             "nombre"=>"Opciones",
@@ -37,12 +40,155 @@ class OptionTableSeeder extends Seeder
             "icono_r"=>""
         ]);
 
+        //4
         Option::create([
             "padre"=>"1",
             "nombre"=>"Roles",
             "ruta"=>"admin/rols",
             "descripcion"=>"Administración de los roles de los usuarios",
             "orden"=>"3",
+            "icono_r"=>""
+        ]);
+
+        //5
+        Option::create([
+            "nombre"=>"Inventario",
+            "descripcion"=>"",
+            "icono_l"=>"fa-th",
+            "orden"=>"1",
+        ]);
+
+        //6
+        Option::create([
+            "padre"=>"5",
+            "nombre"=>"Categorías",
+            "ruta"=>"inventario/categorias",
+            "descripcion"=>"Administración de las categorías de los articulos",
+            "orden"=>"1",
+            "icono_r"=>""
+        ]);
+
+        //7
+        Option::create([
+            "padre"=>"5",
+            "nombre"=>"Artículos",
+            "ruta"=>"inventario/articulos",
+            "descripcion"=>"Administración de los artículos",
+            "orden"=>"2",
+            "icono_r"=>""
+        ]);
+
+        //8
+        Option::create([
+            "padre"=>"5",
+            "nombre"=>"Unidades de medida",
+            "ruta"=>"inventario/unimeds",
+            "descripcion"=>"Administración de las unidades de medida",
+            "orden"=>"3",
+            "icono_r"=>""
+        ]);
+
+        //9
+        Option::create([
+            "nombre"=>"Compras",
+            "descripcion"=>"",
+            "icono_l"=>"fa-shopping-cart",
+            "orden"=>"1",
+        ]);
+
+        //10
+        Option::create([
+            "padre"=>"9",
+            "nombre"=>"Proveedores",
+            "ruta"=>"compras/proveedores",
+            "descripcion"=>"Administración de proveedores",
+            "orden"=>"1",
+            "icono_r"=>""
+        ]);
+
+        //11
+        Option::create([
+            "padre"=>"9",
+            "nombre"=>"Ingresos",
+            "ruta"=>"compras/ingreso",
+            "descripcion"=>"Ingresos de artículos a inventario",
+            "orden"=>"2",
+            "icono_r"=>""
+        ]);
+
+        //12
+        Option::create([
+            "nombre"=>"Ventas",
+            "descripcion"=>"",
+            "icono_l"=>"fa-laptop",
+            "orden"=>"1",
+        ]);
+
+        //13
+        Option::create([
+            "padre"=>"12",
+            "nombre"=>"Ventas",
+            "ruta"=>"ventas/ventas",
+            "descripcion"=>"",
+            "orden"=>"2",
+            "icono_r"=>""
+        ]);
+
+        //14
+        Option::create([
+            "padre"=>"12",
+            "nombre"=>"Clientes",
+            "ruta"=>"ventas/clientes",
+            "descripcion"=>"",
+            "orden"=>"3",
+            "icono_r"=>""
+        ]);
+
+        //15
+        Option::create([
+            "nombre"=>"Medicamentos",
+            "descripcion"=>"",
+            "icono_l"=>"fa-ambulance",
+            "orden"=>"1",
+        ]);
+
+        //16
+        Option::create([
+            "padre"=>"15",
+            "nombre"=>"Laboratorios",
+            "ruta"=>"medicamentos/laboratorios",
+            "descripcion"=>"",
+            "orden"=>"1",
+            "icono_r"=>""
+        ]);
+
+        //17
+        Option::create([
+            "padre"=>"15",
+            "nombre"=>"Categorías",
+            "ruta"=>"medicamentos/fcategorias",
+            "descripcion"=>"",
+            "orden"=>"2",
+            "icono_r"=>""
+        ]);
+
+        //18
+        Option::create([
+            "padre"=>"15",
+            "nombre"=>"Farmacos",
+            "ruta"=>"medicamentos/farmacos",
+            "descripcion"=>"",
+            "orden"=>"3",
+            "icono_r"=>""
+        ]);
+
+        //18
+        Option::create([
+            "padre"=>"15",
+            "nombre"=>"Medicamentos",
+            "ruta"=>"medicamentos/medicamentos",
+            "descripcion"=>"",
+            "orden"=>"4",
             "icono_r"=>""
         ]);
 
