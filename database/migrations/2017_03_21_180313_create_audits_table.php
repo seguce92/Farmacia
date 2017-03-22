506e779cace93987f9b61284292956fc30585f36
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-21 18:11:45 -0600

    Instal Laravel Auditing (edit migration)

--- database/migrations/2017_03_21_180313_create_audits_table.php
+++ database/migrations/2017_03_21_180313_create_audits_table.php
@@ -26,6 +26,7 @@ class CreateAuditsTable extends Migration
     public function up()
     {
         Schema::create('audits', function (Blueprint $table) {
+            $table->engine= "InnoDB";
             $table->increments('id');
             $table->unsignedInteger('user_id')->nullable();
             $table->string('event');
