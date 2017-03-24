1c943c342f90ed219fc7c41fd5cc63e1caa213e9
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-24 15:19:57 -0600

    layout js y css select2

---
+++ resources/views/layouts/select2_css.blade.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="{{asset("plugins/select2/select2.min.css")}}">
---
+++ resources/views/layouts/select2_js.blade.php
@@ -0,0 +1,2 @@
+<script src="{{asset("/plugins/select2/select2.full.min.js")}}"></script>
+<script src="{{asset("/plugins/select2/i18n/es.js")}}"></script>
