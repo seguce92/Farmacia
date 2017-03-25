b5bf6e07cc91f93a4b110b03d3d07d7f92b6a45d
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-25 12:56:18 -0600

    Cambio campos searchables items

--- app/Repositories/ItemRepository.php
+++ app/Repositories/ItemRepository.php
@@ -11,14 +11,8 @@ class ItemRepository extends BaseRepository
      * @var array
      */
     protected $fieldSearchable = [
-        'nombre',
-        'descripcion',
-        'precio',
-        'imagen',
-        'codigo',
-        'unimed_id',
-        'precio_pro',
-        'iestado_id'
+        'nombre' => 'like',
+        'codigo' => 'like',
     ];
 
     /**
