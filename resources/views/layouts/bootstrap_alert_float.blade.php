d85d04dc842e1e5cd4934a721b29df6cd7a023ad
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-26 10:37:09 -0600

    Creada plantilla para mensajes flotantes de informacion

---
+++ resources/views/layouts/bootstrap_alert_float.blade.php
@@ -0,0 +1,29 @@
+@push('css')
+<style>
+    #floating_alert {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        z-index: 5000;
+    }
+</style>
+@endpush
+@push('scripts')
+<script>
+    $(function () {
+        bootstrap_alert = function (message, alert, timeout) {
+            var div = $(
+                    '<div id="floating_alert" class="alert alert-' + alert + ' fade in">'+
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
+                        message + '&nbsp;&nbsp;' +
+                    '</div>'
+            ).appendTo('body');
+
+
+            setTimeout(function () {
+                $(".alert").alert('close');
+            }, timeout);
+        }
+    })
+</script>
+@endpush
