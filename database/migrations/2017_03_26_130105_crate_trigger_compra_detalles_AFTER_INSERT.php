8ea217eac4661457a3d81964248a8c42cd172789
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-03-26 20:44:21 -0600

    Modificadas migraciones triggers

--- database/migrations/2017_03_26_130105_crate_trigger_compra_detalles_AFTER_INSERT.php
+++ database/migrations/2017_03_26_130105_crate_trigger_compra_detalles_AFTER_INSERT.php
@@ -13,12 +13,13 @@ class CrateTriggerCompraDetallesAFTERINSERT extends Migration
      */
     public function up()
     {
-        DB::unprepared('
+        DB::unprepared("
             CREATE DEFINER = CURRENT_USER TRIGGER `farmacia`.`compra_detalles_AFTER_INSERT` AFTER INSERT ON `compra_detalles` FOR EACH ROW
             BEGIN
+                CALL log_triggers('compra_detalles_AFTER_INSERT',concat('se actualizo el stock del item:',new.item_id));           
                 update items set stock = stock + new.cantidad where items.id = new.item_id;
             END
-        ');
+        ");
     }
 
     /**
