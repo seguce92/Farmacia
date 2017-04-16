82d08c030a3daf89da888a1b299f6c43dd86fd11
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-04-16 11:05:22 -0600

    Creada plantila css table-xtra-condensed

---
+++ resources/views/layouts/xtra_condensed_css.blade.php
@@ -0,0 +1,12 @@
+@push('css')
+<style>
+    .table-xtra-condensed > thead > tr > th,
+    .table-xtra-condensed > tbody > tr > th,
+    .table-xtra-condensed > tfoot > tr > th,
+    .table-xtra-condensed > thead > tr > td,
+    .table-xtra-condensed > tbody > tr > td,
+    .table-xtra-condensed > tfoot > tr > td {
+        padding: 2px;
+    }
+</style>
+@endpush
