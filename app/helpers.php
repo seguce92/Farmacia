754261e8baf51d9c8d1710fbc9bbd6bb0aab7a16
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-04-16 17:53:37 -0600

    Nuevo helper fecha

--- app/helpers.php
+++ app/helpers.php
@@ -6,7 +6,12 @@
  * Time: 2:57 PM
  */
 
-function fechaDb($fecha=NULL, $separador='/', $divisor="-")
+function fechaDb($fecha=NULL, $separador='/', $divisor="-"){
+    $tmp=explode("$separador",$fecha);
+
+    return $tmp[2].$divisor.$tmp[1].$divisor.$tmp[0];
+}
+function fecha($fecha=NULL, $separador='-', $divisor="/")
 {
 
     $tmp=explode("$separador",$fecha);
