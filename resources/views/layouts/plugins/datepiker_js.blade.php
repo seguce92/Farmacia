907d445089fe6d2e14ca4c7f72956d6b34c94336
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-04-16 14:41:01 -0600

    Actualizado bootstrap-datepicker js

--- resources/views/layouts/plugins/datepiker_js.blade.php
+++ resources/views/layouts/plugins/datepiker_js.blade.php
@@ -1,3 +1,4 @@
-<script src="{{asset("/plugins/datepicker/bootstrap-datepicker.js")}}"></script>
+{{--<script src="{{asset("/plugins/datepicker/bootstrap-datepicker.js")}}"></script>--}}
+<script src="{{asset("plugins/datepicker/bootstrap-datepicker.min.js")}}"></script>
 <script src="{{asset("/plugins/datepicker/locales/bootstrap-datepicker.es.js")}}"></script>
 
