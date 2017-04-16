e5a4db1154138aa924a52448ed9c7d46b1bf57cd
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-04-16 12:05:09 -0600

    Cambio migración create venta table

--- database/migrations/2017_03_26_125724_create_ventas_table.php
+++ database/migrations/2017_03_26_125724_create_ventas_table.php
@@ -16,7 +16,7 @@ class CreateVentasTable extends Migration {
 		{
 			$table->integer('id', true);
 			$table->integer('cliente_id')->nullable()->index('fk_venta_cliente1_idx');
-			$table->dateTime('fecha');
+			$table->date('fecha');
 			$table->string('serie', 45);
 			$table->string('numero', 45);
 			$table->integer('vestado_id')->index('fk_ventas_estado_venta1_idx');
