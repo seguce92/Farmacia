1071ae78a786226d707ce12520929f8ba2880b41
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-04-16 15:10:30 -0600

    Creado archivo helper y función fechaDb

---
+++ app/helpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Usuario
+ * Date: 16/04/2017
+ * Time: 2:57 PM
+ */
+
+function fechaDb($fecha=NULL, $separador='/', $divisor="-")
+{
+
+    $tmp=explode("$separador",$fecha);
+
+    return $tmp[2].$divisor.$tmp[1].$divisor.$tmp[0];
+}
