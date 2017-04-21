5f83ab6e22b252264acfa624b9885a27d73ed73e
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-04-21 09:35:12 -0600

    Editado seeder clientes

--- database/seeds/ClientesTableSeeder.php
+++ database/seeds/ClientesTableSeeder.php
@@ -20,14 +20,14 @@ class ClientesTableSeeder extends Seeder
             0 => 
             array (
                 'id' => 1,
-                'nit' => '1234',
-                'nombres' => 'Cliente',
-                'apellidos' => 'Pruebas',
+                'nit' => '',
+                'nombres' => 'C/F      ',
+                'apellidos' => ' ',
                 'direccion' => NULL,
                 'telefono' => NULL,
                 'email' => NULL,
                 'created_at' => '2017-04-17 11:05:46',
-                'updated_at' => '2017-04-17 11:05:46',
+                'updated_at' => '2017-04-21 09:31:38',
                 'deleted_at' => NULL,
             ),
         ));
