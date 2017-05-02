88b3007d683194655ee650ee2b4d5bccbf575321
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-05-02 11:54:35 -0600

    Agregado layout con js y css de fancybox

---
+++ resources/views/layouts/plugins/fancy_box.blade.php
@@ -0,0 +1,6 @@
+@push('css')
+<link rel="stylesheet" href="{{asset('css/jquery.fancybox.min.css')}}">
+@endpush
+@push('scripts')
+<script src="{{asset('js/jquery.fancybox.min.js')}}"></script>
+@endpush
