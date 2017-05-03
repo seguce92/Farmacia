915b239d41a43b2213dcb3b6acd9289eadc365da
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-05-02 18:39:12 -0600

    Corregido error validación de imagen en formato de selección items select2

--- resources/views/components/format_slc2_item.blade.php
+++ resources/views/components/format_slc2_item.blade.php
@@ -8,30 +8,28 @@
     .select2-result__nombre, .select2-result__laboratorio { color: black; font-weight: bold; word-wrap: break-word; line-height: 1; font-size: 16px}
     .select2-result__nombre { margin-bottom: 2px;}
     .select2-result__laboratorio  { font-size: 14px;}
-    .select2-result__precio { margin-right: 1em; }
-    .select2-result__precio, .select2-result__ubicacion {
+    .select2-result__precio,.select2-result__ubicacion { margin-right: 1em; }
+    .select2-result__precio, .select2-result__ubicacion, .select2-result__stock {
         font-weight: bold; margin-bottom: 0px; margin-top: 0px; display: inline-block; color: #4EAF4C; font-size: 22px; padding: 0px;
     }
     .select2-result__contiene { color: #777; word-wrap: break-word; line-height: 1; font-size: 11px}
     .select2-results__option--highlighted .select2-result__nombre,
     .select2-results__option--highlighted .select2-result__laboratorio { color: white; }
     .select2-results__option--highlighted .select2-result__precio,
+    .select2-results__option--highlighted .select2-result__stock,
     .select2-results__option--highlighted .select2-result__ubicacion { color: #F2FC2A; }
     .select2-results__option--highlighted .select2-result__contiene { color: #c6dcef; }
     .select2-results__option{ padding: 2px; }
 </style>
 @endpush
 <div class='select2-result-content clearfix'>"+
-    @if($imagen=='undefined')
-        "<div class='select2-result__avatar'><img src='{!! asset($imagen) !!}' class='img-responsive' alt='Image'></div>"+
-    @else
-        "<div class='select2-result__avatar'><img src='{!! asset('img/avatar_none.png') !!}' class='img-responsive' alt='Image'></div>"+
-    @endif
+    "<div class='select2-result__avatar'><img src='{!! asset($imagen) !!}' class='img-responsive' alt='Image'></div>"+
     "<div class='select2-result__meta'>"+
         "<div class='select2-result__nombre'>{{$nombre or 'nombre'}} / {{$descripcion or 'descripcion'}}</div>"+
         "<div class='select2-result__laboratorio'>{{$laboratorio or ''}}</div>"+
         "<div class='select2-result__precio'><i class='fa fa-money'></i> Q {{$precio}} </div>"+
         "<div class='select2-result__ubicacion'><i class='fa fa-archive'></i> {{$ubicacion or ''}} </div>"+
+        "<div class='select2-result__stock'><i class='fa fa-cubes'></i> {{$stock or ''}} </div>"+
         "<div class='select2-result__contiene'>{{$contiene or ''}}</div>"+
     "</div>"+
 "</div>
